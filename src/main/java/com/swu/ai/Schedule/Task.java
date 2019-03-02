3943ae02e0fa417a7366699b80002a79b314d718
彭健彪 <pengjianbiao@hotmail.com>
2019-03-03 01:21:11 +0800

    使用了 redis ，需要安装 redis 服务器端 我安装了 windows 的 3.2.100.msi 版本 参考 https://www.cnblogs.com/jaign/articles/7920588.html 需要设置 requirepass ouy3849

--- src/main/java/com/swu/ai/Schedule/Task.java
+++ src/main/java/com/swu/ai/Schedule/Task.java
@@ -1,7 +1,9 @@
 package com.swu.ai.Schedule;
 
+import com.swu.ai.Util.RedisUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -22,6 +24,13 @@ import org.springframework.stereotype.Component;
 public class Task {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
+    private final RedisUtil redis;
+
+    @Autowired
+    public Task(RedisUtil redisUtil) {
+        this.redis = redisUtil;
+    }
+
     /**
      * @ Scheduled的几种用法
      * @ Scheduled(fixedRate = 5000) ：上一次开始执行时间点之后5秒再执行
@@ -32,6 +41,14 @@ public class Task {
     @Scheduled(fixedRate = 10000)
     public void logTime() {
         logger.info("定时任务，现在时间：" + System.currentTimeMillis());
+        //存到redis数据库  已经存在就更新token  key值是根据固定字符串+id唯一确定的
+        String USER_REDIS_SESSION = "USER_REDIS_SESSION";
+        redis.set(USER_REDIS_SESSION + ":" + "user", String.valueOf(System.currentTimeMillis()), 1000 * 60 * 30);//30分钟
+    }
 
+    @Scheduled(fixedRate = 20000)
+    public void logTime2() {
+        String USER_REDIS_SESSION = "USER_REDIS_SESSION";
+        logger.info("定时任务，获取redis的值：" + redis.get(USER_REDIS_SESSION + ":" + "user"));
     }
 }
